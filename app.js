69a2373de2de318ba33b3147c6842e91f12f07f7
Mehakdeep Singh <msingh065@mylangara.ca>
2021-12-02 09:13:59 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ var wiki = require('./wiki.js');
 var app = express();
 
 var mongoose = require('mongoose');
-var mongoDBUrl = 'mongodb+srv://Mehakdeep:manidhol@cluster0.cwy5z.mongodb.net/local_library?retryWrites=true&w=majority';
+ 
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://Mehakdeep:manidhol@cluster0.cwy5z.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
